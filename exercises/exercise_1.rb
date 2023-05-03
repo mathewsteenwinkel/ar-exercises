0209c530b6bdfec8ceeddfcbbc6f7364c9042447
Noidea22 <mathewsteenwinkel@gmail.com>
2023-05-02 20:32:40 -0600

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,23 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.create (
+  name: "Burnaby"
+  annual_revenue: 300000
+  carries_mens: true
+  carries_womens: true
+)
+store = Store.create (
+  name: "Richmond"
+  annual_revenue: 126000
+  carries_mens: false
+  carries_womens: true
+)
+store = Store.create (
+  name: "Gastown"
+  annual_revenue: 190000
+  carries_mens: true
+  carries_womens: false
+)
+
+puts Store.count
